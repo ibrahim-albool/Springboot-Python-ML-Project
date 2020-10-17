232d43b36c1578fa084bc69ccfd7f730877e80c0
iBoolGuy <soso_bool@hotmail.com>
2020-10-17 23:38:45 +0300

    converted login from a modal to page, and made it the default route

--- src/main/webapp/app/shared/login/login.component.ts
+++ src/main/webapp/app/shared/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 
 import { LoginService } from 'app/core/login/login.service';
@@ -21,21 +20,12 @@ export class LoginModalComponent implements AfterViewInit {
     rememberMe: [false],
   });
 
-  constructor(private loginService: LoginService, private router: Router, public activeModal: NgbActiveModal, private fb: FormBuilder) {}
+  constructor(private loginService: LoginService, private router: Router, private fb: FormBuilder) {}
 
   ngAfterViewInit(): void {
     if (this.username) {
       this.username.nativeElement.focus();
     }
-  }
-
-  cancel(): void {
-    this.authenticationError = false;
-    this.loginForm.patchValue({
-      username: '',
-      password: '',
-    });
-    this.activeModal.dismiss('cancel');
   }
 
   login(): void {
@@ -48,13 +38,12 @@ export class LoginModalComponent implements AfterViewInit {
       .subscribe(
         () => {
           this.authenticationError = false;
-          this.activeModal.close();
           if (
             this.router.url === '/account/register' ||
             this.router.url.startsWith('/account/activate') ||
             this.router.url.startsWith('/account/reset/')
           ) {
-            this.router.navigate(['']);
+            this.router.navigate(['/home']);
           }
         },
         () => (this.authenticationError = true)
@@ -62,12 +51,10 @@ export class LoginModalComponent implements AfterViewInit {
   }
 
   register(): void {
-    this.activeModal.dismiss('to state register');
     this.router.navigate(['/account/register']);
   }
 
   requestResetPassword(): void {
-    this.activeModal.dismiss('to state requestReset');
     this.router.navigate(['/account/reset', 'request']);
   }
 }
