ce52fe7e74df61c817b58fdf188884f4f2c9ba41
iBoolGuy <soso_bool@hotmail.com>
2020-10-24 06:52:32 +0300

    * Conplemted predict-new-data page, now it returns correct server status with specific details. * Added a loading bar gif. * Fixed broken styling.

--- src/main/webapp/app/admin/predict-new-data/predict-new-data.component.ts
+++ src/main/webapp/app/admin/predict-new-data/predict-new-data.component.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
 import { PredictNewData } from 'app/shared/model/predict-new-data.model';
 import { PredictNewDataService } from './predict-new-data.service';
 
@@ -11,9 +13,12 @@ import { PredictNewDataService } from './predict-new-data.service';
 export class PredictNewDataComponent implements OnInit {
   predictDataFile!: PredictNewData;
   authorities: string[] = [];
-  isSaving = false;
-  fileNameTemp: any;
 
+  isSaving = false;
+  success = false;
+  error = false;
+  error5001 = false;
+  error5002 = false;
   predictDataForm = this.fb.group({
     fileName: [''],
   });
@@ -32,16 +37,16 @@ export class PredictNewDataComponent implements OnInit {
     });
   }
 
-  previousState(): void {
-    window.history.back();
-  }
-
   save(): void {
     this.isSaving = true;
-    this.updatePredictData(this.predictDataFile);
-    this.predictNewDataService.predictData(this.fileNameTemp, this.predictDataFile).subscribe(
+    this.predictDataForm.get('fileName')?.disable();
+    this.error = false;
+    this.error5001 = false;
+    this.error5002 = false;
+    const fileNameTemp = this.predictDataForm.get(['fileName'])!.value;
+    this.predictNewDataService.predictData(fileNameTemp).subscribe(
       () => this.onSaveSuccess(),
-      () => this.onSaveError()
+      response => this.onSaveError(response)
     );
   }
 
@@ -51,17 +56,25 @@ export class PredictNewDataComponent implements OnInit {
     });
   }
 
-  private updatePredictData(predictDataFile: PredictNewData): void {
-    predictDataFile.fileName = this.predictDataForm.get(['fileName'])!.value;
-    this.fileNameTemp = predictDataFile.fileName;
-  }
-
   private onSaveSuccess(): void {
     this.isSaving = false;
-    this.previousState();
+    this.success = true;
+    this.predictDataForm.get('fileName')?.enable();
   }
 
-  private onSaveError(): void {
+  private onSaveError(response: HttpErrorResponse): void {
     this.isSaving = false;
+    this.predictDataForm.get('fileName')?.enable();
+    if (response.status === 500 && response.error.detail === 'No value present') {
+      this.error5001 = true;
+    } else if (
+      response.status === 500 &&
+      response.error.detail ===
+        'I/O error on GET request for "http://localhost:5000/predictXNew": Connection refused: connect; nested exception is java.net.ConnectException: Connection refused: connect'
+    ) {
+      this.error5002 = true;
+    } else {
+      this.error = true;
+    }
   }
 }
--- src/main/webapp/app/admin/predict-new-data/predict-new-data.service.ts
+++ src/main/webapp/app/admin/predict-new-data/predict-new-data.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { SERVER_API_URL } from 'app/app.constants';
-import { createRequestOption } from 'app/shared/util/request-util';
 import { IPredictNewData } from 'app/shared/model/predict-new-data.model';
 
 type EntityResponseType = HttpResponse<IPredictNewData>;
@@ -15,33 +14,12 @@ export class PredictNewDataService {
 
   constructor(protected http: HttpClient) {}
 
-  predictData(newDataFile: string, predictDataModel: IPredictNewData): Observable<EntityResponseType> {
-    return this.http.post<IPredictNewData>(`${this.resourceUrl}/${newDataFile}`, predictDataModel, { observe: 'response' });
+  predictData(newDataFile: string): Observable<{}> {
+    return this.http.post(this.resourceUrl, null, {
+      params: new HttpParams().set('newDataFile', newDataFile),
+    });
   }
   authorities(): Observable<string[]> {
     return this.http.get<string[]>(SERVER_API_URL + 'api/users/authorities');
   }
-  // POST: /api/predictData
-  // http://localhost:9000/api/predictData?newDataFile=teachers_test_300.xlsx
-  // takes in the path variables:
-  // newDataFile
-  // returns Status 200 with the body Prediction succeeded. in the success scenario.
-  // Status 500 in the failure scenario, when data in the model is not present.
-  // {
-  //     "type": "https://www.jhipster.tech/problem/problem-with-message",
-  //     "title": "Internal Server Error",
-  //     "status": 500,
-  //     "detail": "No value present",
-  //     "path": "/api/predictData",
-  //     "message": "error.http.500"
-  // }
-  // or 500 with the json when python server is down:
-  // {
-  //     "type": "https://www.jhipster.tech/problem/problem-with-message",
-  //     "title": "Internal Server Error",
-  //     "status": 500,
-  //     "detail": "I/O error on GET request for \"http://localhost:5000/predictXNew\": Connection refused: connect; nested exception is java.net.ConnectException: Connection refused: connect",
-  //     "path": "/api/predictData",
-  //     "message": "error.http.500"
-  // }
 }
