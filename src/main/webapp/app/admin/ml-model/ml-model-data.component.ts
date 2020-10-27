39f5f8467f46bea85ee446843c5d127c2df0e749
iBoolGuy <soso_bool@hotmail.com>
2020-10-27 03:36:50 +0200

    Stepping stones for the model view page. Not ready at all. Committing the work cause why not.

--- src/main/webapp/app/admin/ml-model/ml-model-data.component.ts
+++ src/main/webapp/app/admin/ml-model/ml-model-data.component.ts
@@ -1,31 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
-import { FormBuilder } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-
-import { IMLModel } from 'app/shared/model/ml-model.model';
-import { MLModelService } from './ml-model.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'jhi-ml-model-data',
-  templateUrl: './ml-model-data.component.html',
+  template: '<plotly-plot [data]="graph.data" [layout]="graph.layout"></plotly-plot>',
 })
-export class MlModelDataComponent implements OnInit {
-  authorities: string[] = [];
-
-  constructor(private mLModelService: MLModelService, private route: ActivatedRoute, private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.mLModelService.authorities().subscribe(authorities => {
-      this.authorities = authorities;
-    });
-  }
-
-  // save(): void {
-  //   const fileNameTemp = this.predictDataForm.get(['fileName'])!.value;
-  //   this.predictNewDataService.predictData(fileNameTemp).subscribe(
-  //     () => this.onSaveSuccess(),
-  //     response => this.onSaveError(response)
-  //   );
-  // }
+export class MlModelDataComponent {
+  public graph = {
+    data: [
+      { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: { color: 'red' } },
+      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+    ],
+    layout: { width: 320, height: 240, title: 'A Fancy Plot' },
+  };
 }
+
+// import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+// declare var dataPlot: any;
+
+// @Component({
+//   selector: 'jhi-ml-model-data',
+//   templateUrl: './ml-model-data.component.html',
+// })
+// export class MlModelDataComponent implements OnInit {
+//   @ViewChild('div1') div1!: QueryList<ElementRef>;
+//   @ViewChild('div2') div2!: QueryList<ElementRef>;
+
+//   myData: any;
+//   constructor(div1: QueryList<ElementRef>, div2: QueryList<ElementRef>) {
+//     div1.first.nativeElement.getAttribute('id');
+//     div2.last.nativeElement.getAttribute('id');
+//   }
+
+//   ngOnInit(): void {
+
+//     this.myData = {
+//       "loss": [
+//         0.6437230565968681,
+//         0.013746686827610521,
+//         0.015016184188425541
+//       ],
+//       "accuracy": [
+//         0.6740923523902893,
+//         0.9039342403411865,
+//         0.9947881698608398,
+//         0.9943200945854187
+//       ]
+//     }
+//     this.funssss();
+//   }
+
+//   funssss(): void {
+//     dataPlot(this.myData);
+//     dataPlot.divs(this.div1.first.nativeElement.getAttribute('id'), this.div2.last.nativeElement.getAttribute('id'));
+//   }
+// }
