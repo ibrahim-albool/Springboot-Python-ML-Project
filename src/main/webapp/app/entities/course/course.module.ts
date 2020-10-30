c6b325fcfc4a9c9b07ed9362b438dafe4c6f8f58
iBoolGuy <soso_bool@hotmail.com>
2020-10-30 07:40:26 +0200

    Added search filters.

--- src/main/webapp/app/entities/course/course.module.ts
+++ src/main/webapp/app/entities/course/course.module.ts
@@ -7,9 +7,10 @@ import { CourseDetailComponent } from './course-detail.component';
 import { CourseUpdateComponent } from './course-update.component';
 import { CourseDeleteDialogComponent } from './course-delete-dialog.component';
 import { courseRoute } from './course.route';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [TeachPredictorSharedModule, RouterModule.forChild(courseRoute)],
+  imports: [FormsModule, TeachPredictorSharedModule, RouterModule.forChild(courseRoute)],
   declarations: [CourseComponent, CourseDetailComponent, CourseUpdateComponent, CourseDeleteDialogComponent],
   entryComponents: [CourseDeleteDialogComponent],
 })
